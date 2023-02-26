27c97843eaa772f2dc4da4d0e9f238b30c48da62
DIEGO ALEXANDER VALENCIA CALDER�N <diegovalencia313667@correo.itm.edu.co>
2023-02-26 14:16:32 -0500

    I created my first code with new Date Class

---
+++ MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace MyFirstCodeOOP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var dateObject = new Date(2002, 12, 3);                    //Notipado de C#, para saber el tipo de dato que recibe la variable, aparte necesita el número de variables que debo traer
+                Console.WriteLine(dateObject);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
