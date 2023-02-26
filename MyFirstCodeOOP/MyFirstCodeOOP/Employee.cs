737bb9cfcc61d22131588b8c23d4c1e91b512e01
DIEGO ALEXANDER VALENCIA CALDER�N <diegovalencia313667@correo.itm.edu.co>
2023-02-26 15:19:52 -0500

    Creating new concret class called SalaryEmployee

--- MyFirstCodeOOP/MyFirstCodeOOP/Employee.cs
+++ MyFirstCodeOOP/MyFirstCodeOOP/Employee.cs
@@ -21,14 +21,28 @@ namespace MyFirstCodeOOP
 
         public override string ToString()
         {
-            return $"------EMPLOYEE------\n\t " +                      //Son saltos de línea
-                $"ID: {Id} \n\t " +
-                $"FirstName: {FirstName} \n\t " +
-                $"LastName: {LastName} \n\t " +
-                $"BirthDate: {BirthDate} \n\t " +
-                $"HiringDate: {HiringDate} \n\t " +
-                $"IsActive?: {IsActive}";              
-        
+            return String.Format(                                   //Mejor que la extrapolación de $
+                "-------EMPLOYEE-------\n\t" +
+                "Id.................{0}\n\t" +
+                "Last Name..........{2}\n\t" +
+                "First Name.........{1}\n\t" +
+                "Birth Day..........{3}\n\t" +
+                "Hiring Date........{4}\n\t" +
+                "Is Active?.........{5}",
+                Id,
+                FirstName,
+                LastName,
+                BirthDate,
+                HiringDate,
+                IsActive);
+
+            //return $"-----EMPLOYEE------\n\t " +                      //Son saltos de línea
+            //    $"ID: {Id} \n\t " +
+            //    $"FirstName: {FirstName} \n\t " +
+            //    $"LastName: {LastName} \n\t " +
+            //    $"BirthDate: {BirthDate} \n\t " +
+            //    $"HiringDate: {HiringDate} \n\t " +
+            //    $"IsActive?: {IsActive}";      
         }
 
         #endregion
---
+++ MyFirstCodeOOP/MyFirstCodeOOP/SalaryEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstCodeOOP
+{
+    public class SalaryEmployee : Employee
+    {
+        #region Properties
+
+        public decimal Salary { get; set; }
+
+        #endregion
+
+        #region Methods
+        public override decimal GetValueToPay()
+        {
+            return Salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} \n\t Value to Pay:{GetValueToPay()}";              //Se trae el método con el base.ToString(), base, papá o clase base, y por ahí derecho la concatena con el valor a pagar
+            
+        }
+
+        #endregion
+    }
+}
