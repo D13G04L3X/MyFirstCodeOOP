d3cbfa1b6424baac2ebdb83ed9d6ca35fa9697ba
DIEGO ALEXANDER VALENCIA CALDER�N <diegovalencia313667@correo.itm.edu.co>
2023-02-26 17:31:50 -0500

    New CommissionEmployee class created

---
+++ MyFirstCodeOOP/MyFirstCodeOOP/CommissionEmployee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstCodeOOP
+{
+    public class CommissionEmployee : Employee              // Se hace la herencia de la clase
+    {
+        #region Properties
+        public float CommissionPercentage { get; set; }
+        public decimal Sales { get; set; }          //Ventas
+
+        #endregion
+
+        #region Methods
+        public override decimal GetValueToPay()
+        {
+            return Sales * (decimal)ConvertPercentage(CommissionPercentage);
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tCommission Percentage......{ConvertPercentage(CommissionPercentage):P2}" +
+                $"\n\tSales......................{Sales:C2}" +                           //Currency, valor monetario de 2 cifras
+                $"\n\tValue to pay...............{GetValueToPay():C2}";
+        }
+
+        public float ConvertPercentage(double commissionPercentage)             //Requirimiento de porcentaje
+        {
+            return (CommissionPercentage/100);
+        }
+        #endregion
+    }
+}
--- MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
+++ MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
@@ -26,25 +26,83 @@ namespace MyFirstCodeOOP
                 var day = Console.ReadLine();
                 Console.WriteLine("");
                 Console.WriteLine(new Date(Convert.ToInt32(year), Convert.ToInt32(month), Convert.ToInt32(day)));
+                
                 Console.WriteLine("");
-
                 Console.WriteLine("*********Testing latest implementation*******");
                 Console.WriteLine("");
 
-                Employee salaryEmployee = new SalaryEmployee()    //instacnciar el objeto de la clase hija a la abstracta
+                Console.WriteLine("*******************");
+                Console.WriteLine("* SALARY EMPLOYEE *");
+                Console.WriteLine("*******************");
+
+                Console.WriteLine("Type your ID");
+                int id = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Type your first name");
+                string firstName = Console.ReadLine();
+
+                Console.WriteLine("Type your last name");
+                string lastName = Console.ReadLine();
+
+                Console.WriteLine("Are you active?");
+                bool isActive = Convert.ToBoolean(Console.ReadLine());
+
+                Console.WriteLine("Enter your salary:");
+                decimal salary = Convert.ToDecimal(Console.ReadLine());
+
+                Employee salaryEmployee = new SalaryEmployee()    // Instanciar el objeto de la clase hija a la abstracta
                 {
-                    Id = Convert.ToInt32(Console.ReadLine()),
-                    FirstName = Console.ReadLine(),
-                    LastName = Console.ReadLine(),
+                    Id = id,
+                    FirstName = firstName,
+                    LastName = lastName,
                     BirthDate = new Date(Convert.ToInt32(year), Convert.ToInt32(month), Convert.ToInt32(day)),
                     HiringDate = new Date(2022, 3, 4),
-                    IsActive = Convert.ToBoolean(Console.ReadLine()),
-                    Salary = Convert.ToDecimal(Console.ReadLine())            // Convierte el double a un decimal con la M al final
+                    IsActive = isActive,
+                    Salary = salary                                 // Convierte el double a un decimal con la M al final
 
                 };
 
-                Console.WriteLine(salaryEmployee);              // Va a dibujar lo que se hizo en la creación del objeto anterior 
+                // Console.WriteLine(salaryEmployee);              // Va a dibujar lo que se hizo en la creación del objeto anterior 
 
+                Console.WriteLine("");
+
+                Console.WriteLine("***********************");
+                Console.WriteLine("* COMMISSION EMPLOYEE *");
+                Console.WriteLine("***********************");
+
+                Console.WriteLine("Type your ID");
+                id = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Type your first name");
+                firstName = Console.ReadLine();
+
+                Console.WriteLine("Type your last name");
+                lastName = Console.ReadLine();
+
+                Console.WriteLine("Are you active?");
+                isActive = Convert.ToBoolean(Console.ReadLine());
+
+                Console.WriteLine("Enter your commission percentage:");
+                float commissionPercentage = Convert.ToSingle(Console.ReadLine());
+
+                Console.WriteLine("Enter your sales:");
+                decimal sales = Convert.ToDecimal(Console.ReadLine());
+
+                Employee commissionEmployee = new CommissionEmployee()                      // Creación del objeto
+                {
+                    Id = id,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    BirthDate = new Date(Convert.ToInt32(year), Convert.ToInt32(month), Convert.ToInt32(day)),
+                    HiringDate = new Date(2022, 3, 4),
+                    IsActive = isActive,
+                    CommissionPercentage = commissionPercentage,
+                    Sales = sales,
+                };
+
+                //Console.WriteLine(commissionEmployee);
+
+                Console.WriteLine("");
             }
             catch (Exception ex)
             {
